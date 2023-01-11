738b6716ce3daff3b94041f6a82d654277b61887
Brian Simiyu <briansimiyuj@gmail.com>
2023-01-11 14:28:11 +0300

    Auto Compile

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 console.log('Hello World!');
+var myName = 'Brian Simiyu';
+console.log(myName);
 // Compile TypeScript to JavaScript
 // tsc ${fileName}.ts ${fileName}.js
 // If the files has the same name, use ${fileName}.ts 
+// To auto compile use ${fileName}.ts -w
--- script.ts
+++ script.ts
@@ -1,7 +1,13 @@
 console.log('Hello World!')  
 
+const myName = 'Brian Simiyu'
+
+console.log(myName) 
+
 // Compile TypeScript to JavaScript
 
 // tsc ${fileName}.ts ${fileName}.js
 
 // If the files has the same name, use ${fileName}.ts 
+
+// To auto compile use ${fileName}.ts -w
