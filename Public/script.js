d4b129207a80dfbe53090b55bf0b3840b7aca8c9
Brian Simiyu <briansimiyuj@gmail.com>
2023-01-14 09:37:50 +0300

    tsconfig file

--- Public/script.js
+++ Public/script.js
@@ -1,11 +1,5 @@
-var ninja;
-ninja = {
-    name: "ninja",
-    age: 30
-};
-console.log(ninja);
-ninja = {
-    name: 30,
-    age: "ninja"
-};
-console.log(ninja);
+"use strict";
+// Use `tsc --init` command to initialize tsconfig.json file
+// In tsconfig.json file, set "rootDir": "src" and "outDir": "public"
+// Use `tsc` command to compile TypeScript to JavaScript or `tsc -w` command to auto compile
+console.log('working');
--- SRC/script.ts
+++ SRC/script.ts
@@ -1,24 +1,8 @@
-let ninja: { name: any, age: any}
+// Use `tsc --init` command to initialize tsconfig.json file
+
+// In tsconfig.json file, set "rootDir": "src" and "outDir": "public"
+
+// Use `tsc` command to compile TypeScript to JavaScript or `tsc -w` command to auto compile
 
 
-ninja = {
-
-    name: "ninja",
-    
-    age: 30
-
-}
-
-console.log(ninja)
-
-
-
-ninja = {
-
-    name: 30,
-    
-    age: "ninja"
-
-}
-
-console.log(ninja)
+console.log('working')
