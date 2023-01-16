1b0f7803e285d0324afca92060b0f9b68e512449
Brian Simiyu <briansimiyuj@gmail.com>
2023-01-16 13:29:12 +0300

    Type Casting 2

--- Public/script.js
+++ Public/script.js
@@ -1,4 +1,7 @@
 "use strict";
 // const form = document.querySelector("form")!
-const form = document.querySelector(".new-item-form");
-console.log(form.children);
+const form = document.querySelector(".new-item-form"), type = document.querySelector("#type"), toFrom = document.querySelector("#tofrom"), details = document.querySelector("#details"), amount = document.querySelector("#amount");
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log(type.value, toFrom.value, details.value, amount.valueAsNumber);
+});
--- SRC/script.ts
+++ SRC/script.ts
@@ -1,5 +1,31 @@
 // const form = document.querySelector("form")!
 
-const form = document.querySelector(".new-item-form") as HTMLFormElement
+const form = document.querySelector(".new-item-form") as HTMLFormElement,
 
-console.log(form.children)
+   type = document.querySelector("#type") as HTMLSelectElement,
+
+   toFrom = document.querySelector("#tofrom") as HTMLInputElement,
+
+   details = document.querySelector("#details") as HTMLInputElement,
+
+   amount = document.querySelector("#amount") as HTMLInputElement
+
+
+
+form.addEventListener("submit", (e: Event) =>{
+
+   e.preventDefault()
+
+   console.log(
+
+      type.value,
+
+      toFrom.value,
+
+      details.value,
+
+      amount.valueAsNumber
+
+   )
+
+})
