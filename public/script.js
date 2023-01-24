d7ba58ffd97d1d879d9bbd0814750a3297e77182
Brian Simiyu <briansimiyuj@gmail.com>
2023-01-24 08:55:51 +0300

    Enums

--- public/script.js
+++ public/script.js
@@ -1,2 +1,9 @@
 "use strict";
-let users = [650396, 'Brian'];
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 1] = "Small";
+    Size[Size["Medium"] = 2] = "Medium";
+    Size[Size["Large"] = 3] = "Large";
+})(Size || (Size = {}));
+let mySize = Size.Medium;
+console.log(mySize);
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,13 @@
-let users: [number, string] = [650396, 'Brian']
+/* const small = 1,
+
+    medium = 2,
+
+    large = 3 */
+
+
+enum Size { Small = 1, Medium, Large }
+
+let mySize: Size = Size.Medium
+
+console.log(mySize)
+
