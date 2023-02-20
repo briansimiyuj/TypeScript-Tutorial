8f3585cba3f9d59fa2b7ff85bd63dc3ffb434314
Brian Simiyu <briansimiyuj@gmail.com>
2023-02-20 16:40:58 +0300

    Classes

--- public/script.js
+++ public/script.js
@@ -1,2 +1,9 @@
 "use strict";
-console.log('Hello World!');
+class User {
+    constructor(email, name) {
+        this.city = '';
+        this.email = email;
+        this.name = name;
+    }
+}
+const brian = new User('brian@techfybiz.co.ke', 'Brian');
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,22 @@
-console.log('Hello World!')
+class User{
+
+    email: string
+
+    name: string
+
+    readonly city: string = ''
+
+    constructor(email: string, name:string){
+
+        this.email = email
+
+        this.name = name
+
+    }
+
+}
+
+
+
+
+const brian = new User('brian@techfybiz.co.ke', 'Brian')
