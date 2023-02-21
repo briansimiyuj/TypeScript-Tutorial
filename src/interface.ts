83c08eed0923154757266a239327cd12061407a0
Brian Simiyu <briansimiyuj@gmail.com>
2023-02-21 13:27:19 +0300

    Class implements interfaces

---
+++ src/interface.ts
@@ -0,0 +1,60 @@
+interface TakePhoto{
+
+    cameraMode: string
+
+    filter: string
+
+    burst: number
+
+}
+
+
+
+interface Stories{
+
+    createStory(): void
+
+}
+
+
+
+class Instagram implements TakePhoto{
+
+    constructor(
+
+        public cameraMode: string,
+        
+        public filter: string,
+        
+        public burst: number
+
+    ){}
+    
+
+}
+
+
+
+
+class YouTube implements TakePhoto, Stories{
+
+    constructor(
+
+        public cameraMode: string,
+
+        public filter: string,
+
+        public burst: number,
+
+        public shorts: string
+
+    ){}
+
+
+    createStory(): void {
+        
+        console.log('Story created')
+
+    }
+
+}
