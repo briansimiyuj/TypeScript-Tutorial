aa1cacfcac0cd8f9eede609e229d6c3576f23ed9
Brian Simiyu <briansimiyuj@gmail.com>
2023-02-21 10:29:17 +0300

    Setter

--- public/script.js
+++ public/script.js
@@ -4,10 +4,20 @@ class User {
         this.email = email;
         this.name = name;
         this.userID = userID;
+        this._courseCount = 1;
         this.city = 'Nairobi';
     }
     get getAppleEmail() {
         return `apple${this.email}`;
+    }
+    get getCourseCount() {
+        return this._courseCount;
+    }
+    set getCourseCount(courseNumber) {
+        if (courseNumber <= 1) {
+            throw new Error('Course number must be greater than 1');
+        }
+        this._courseCount = courseNumber;
     }
 }
 const brian = new User('brian@techfybiz.co.ke', 'Brian', 1234765);
--- src/script.ts
+++ src/script.ts
@@ -1,5 +1,7 @@
 class User{
 
+    private _courseCount = 1
+
     readonly city: string = 'Nairobi'
 
     constructor(
@@ -18,6 +20,28 @@ class User{
 
         return `apple${this.email}`
 
+    }
+
+
+
+    get getCourseCount(): number{
+
+        return this._courseCount
+
+    }
+
+
+
+    set getCourseCount(courseNumber){
+
+        if(courseNumber <= 1){
+
+            throw new Error('Course number must be greater than 1')
+
+        }
+
+        this._courseCount = courseNumber
+        
     }
 
 }
