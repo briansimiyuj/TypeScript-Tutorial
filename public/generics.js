4c5c82e62092058779c0ae04e1e49300b856c170
Brian Simiyu <briansimiyuj@gmail.com>
2023-02-23 13:07:09 +0300

    Generics

---
+++ public/generics.js
@@ -0,0 +1,15 @@
+"use strict";
+const score = [];
+const names = [];
+function identityOne(val) {
+    return val;
+}
+function identityTwo(val) {
+    return val;
+}
+function identityThree(val) {
+    return val;
+}
+identityThree(3);
+identityThree('Hello');
+identityThree(true);
